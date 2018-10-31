fdef1df8a1e84a69bb74a2759ccb33b0368a67dc
zhuhj <zhuhjtcx@qq.com>
2018-10-31 22:31:38 +0800

    fsfsfs

--- demo13_yield.py
+++ demo13_yield.py
@@ -1,7 +1,20 @@
 # coding=utf-8
 '''
 1. only self or 1 /
-2.
+2.1不是素数
 '''
+def is_p(t_int):
+    if t_int == 1:
+        return False
+    elif t_int ==2 :
+        return True
+    else:
+        for x in xrange(2,t_int):
+            if t_int%2==0:
+                return False
+        return True
+
+
+print(is_p(3))
 
 
